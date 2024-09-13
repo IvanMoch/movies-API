9ecdd699ee16d64353bb341ab3acd86bc5a9f2d2
IvanMoch <mochniyivan@gmail.com>
2024-09-13 17:37:56 -0400

    ...

--- Models/MovieModel.mjs
+++ Models/MovieModel.mjs
@@ -2,8 +2,8 @@ import mysql from 'mysql2/promise'
 import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } from '../config.mjs'
 
 const config = {
-  host: DB_HOST,
   port: DB_PORT,
+  host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME
