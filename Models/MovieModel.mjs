0fac6415533b2381a969acdad5831ba4f9f8bea3
IvanMoch <mochniyivan@gmail.com>
2024-09-13 21:20:23 -0400

    changing the create connection to a pool connection

--- Models/MovieModel.mjs
+++ Models/MovieModel.mjs
@@ -1,25 +1,26 @@
-import mysql from 'mysql2/promise'
-import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } from '../config.mjs'
+//import mysql from 'mysql2/promise'
+//import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } from '../config.mjs'
+import { pool } from '../db.mjs'
 
-const config = {
+/*const config = {
   port: DB_PORT,
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME
-}
+}*/
 
-const connection = await mysql.createConnection(config)
+//const connection = await mysql.createConnection(config)
 
 export class MovieModel{
     static async getAll() {
-        const [movies] = await connection.query('select BIN_TO_UUID(id) as id, title, director, duration, poster, rate from movies.movie ')
+        const [movies] = await pool.query('select BIN_TO_UUID(id) as id, title, director, duration, poster, rate from movies.movie ')
         return movies
     }
   
   static async getBydID({ id }) {
 
-    const [movie] = await connection.query('select BIN_TO_UUID(id) as id, title, director, duration, poster, rate from movies.movie where BIN_TO_UUID(id) = ? ', [id])
+    const [movie] = await pool.query('select BIN_TO_UUID(id) as id, title, director, duration, poster, rate from movies.movie where BIN_TO_UUID(id) = ? ', [id])
     
     if (movie.length == 0) {
       return false
@@ -29,14 +30,14 @@ export class MovieModel{
   }
 
   static async createMovie({ newMovie }) {
-    const [uuidResult] = await connection.query('select UUID() uuid')
+    const [uuidResult] = await pool.query('select UUID() uuid')
     const [{ uuid }] = uuidResult
     const movie = {
       id: uuid,
       ...newMovie.data
     }
 
-    await connection.query(`
+    await pool.query(`
       insert into movies.movie(id,title,director,duration,poster,rate)
       values(UUID_TO_BIN(?),?,?,?,?,?)`, [movie.id, movie.title, movie.director, movie.duration, movie.poster, movie.rate])
     
@@ -45,13 +46,13 @@ export class MovieModel{
 
   static async updateMovie({ newData, id }) {
 
-  const [movie] = await connection.query('select BIN_TO_UUID(id) as id, title, director, duration, poster, rate from movies.movie where BIN_TO_UUID(id) = ? ', [id])    
+  const [movie] = await pool.query('select BIN_TO_UUID(id) as id, title, director, duration, poster, rate from movies.movie where BIN_TO_UUID(id) = ? ', [id])    
     if (movie[0]) {
       const modifiedMovie = {
         ...movie[0],
         ...newData.data
       }
-    await connection.query(`
+    await pool.query(`
       update movies.movie
       set title = ?, director = ?, duration = ?, poster = ?, rate = ?
       where BIN_TO_UUID(id) = ?`, [modifiedMovie.title, modifiedMovie.director, modifiedMovie.duration, modifiedMovie.poster, modifiedMovie.rate, id])
